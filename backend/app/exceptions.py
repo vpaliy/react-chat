cb11d4ecd7aeb8ecce497d59c67aa3ed6ee31786
thevpaliy <vpaliy97@gmail.com>
2018-12-07 12:04:16 -0500

    Update users

---
+++ backend/app/exceptions.py
@@ -0,0 +1,31 @@
+from flask import jsonify
+
+
+def template(message=str(), code=500):
+    return {'message': message, 'status_code': code}
+
+USER_NOT_FOUND = template('User not found', code=404)
+USER_ALREADY_REGISTERED = template('User already registered', code=422)
+UNKNOWN_ERROR = template(code=500)
+
+
+class InvalidUsage(Exception):
+  def __init__(self, message, status_code=None, payload=None):
+    Exception.__init__(self)
+    self.message = message
+    if status_code is not None:
+      self.status_code = status_code
+    self.payload = payload
+
+  def to_json(self):
+    rv = dict(self.payload or ())
+    rv['message'] = self.message
+    return jsonify(rv)
+
+  @classmethod
+  def user_not_found(cls):
+    return cls(**USER_NOT_FOUND)
+
+  @classmethod
+  def user_already_registered(cls):
+    return cls(**USER_ALREADY_REGISTERED)
--- backend/app/user/views.py
+++ backend/app/user/views.py
@@ -3,23 +3,34 @@ from app.user import users
 from app.database import db
 from app.user.models import User
 from flask_apispec import use_kwargs, marshal_with
+from app.exceptions import InvalidUsage
+from sqlalchemy.exc import IntegrityError
 
 
 @users.route('/api/users/register', methods=('POST',))
 @use_kwargs(schema)
 @marshal_with(schema)
 def register(username, password, email, **kwargs):
-  user = User(
-    email=email,
-    username=username,
-    password=password,
-    **kwargs
-  )
+  try:
+    user = User(
+      email=email,
+      username=username,
+      password=password,
+      **kwargs
+    ).save()
+  except IntegrityError:
+    db.session.rollback()
+    raise InvalidUsage.user_already_registered()
   return user
 
 
 @users.route('/api/users/login', methods=('POST',))
 @use_kwargs(schema)
 @marshal_with(schema)
-def login(username, password):
-  pass
+def login(username, password, **kwargs):
+  user = User.query.filter_by(username=username).first()
+  if user is None:
+    user = User.query.filter_by(email=username).first()
+  if user is not None and user.check_password(password):
+    return user
+  raise InvalidUsage.user_not_found()
